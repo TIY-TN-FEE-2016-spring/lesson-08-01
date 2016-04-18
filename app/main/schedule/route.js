f56397d18ca8b6e682271f8af42bee758ab49bc1
Ryan Tablada <ryan.tablada@gmail.com>
2016-04-18 11:27:43 -0500

    Update to show what :team_id is doing

--- app/main/schedule/route.js
+++ app/main/schedule/route.js
@@ -1,4 +1,8 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  // Same as saying :team_id
+  // model(params) {
+  //   return this.store.findRecord(`team`, params.team_id);
+  // },
 });
